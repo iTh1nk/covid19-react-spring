99e099d6c44f72d61feff44a561633a33b896998
iTh1nk <fnchao@hotmail.com>
2020-04-02 13:06:02 -0700

    port change dp aws

--- portfolio-spring-react/src/components/Intro.js
+++ portfolio-spring-react/src/components/Intro.js
@@ -1,26 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import Axios from 'axios';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
-export default function Intro () {
-  const [getSpring, setGetSpring] = useState("None");
+export default function Intro() {
+  const [getSpring, setGetSpring] = useState([]);
 
-  useEffect (() => {
-    Axios
-      .get("/")
+  useEffect(() => {
+    Axios.get("/api/categories")
       .then(resp => {
-        // setGetSpring(resp.data);
-        console.log("Get from Spring: ", resp);
+        setGetSpring(resp.data);
+        console.log("Get from Spring: ", resp.data);
       })
       .catch(err => {
         console.log(err);
       });
-  }, [])
+  }, []);
 
   return (
     <>
-      <div>
-        Introduction: {getSpring}
-      </div>
+      <div>Introduction: {getSpring.map(item => (
+        <div key={item.id}>{item.name}</div>
+      ))}</div>
     </>
-  )
+  );
 }
