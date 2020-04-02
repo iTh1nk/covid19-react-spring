61229c913a74504f64a86e60a7f715c316169619
iTh1nk <fnchao@hotmail.com>
2020-04-02 15:53:45 -0700

    covid19.js

--- covid19-react/src/components/Covid19.js
+++ covid19-react/src/components/Covid19.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
 
 export default function Covid19 () {
-  return "Covid-19 Updates..."
+  return "麦速更新中..."
 }
--- covid19-react/src/components/Navi.js
+++ covid19-react/src/components/Navi.js
@@ -5,12 +5,12 @@ function Navi() {
   return (
     <>
       <Navbar bg="light" variant="light">
-        <Navbar.Brand href="#home">Chao</Navbar.Brand>
-        <Nav className="mr-auto">
+        <Navbar.Brand href="#home">麦报</Navbar.Brand>
+        {/* <Nav className="mr-auto">
           <Nav.Link href="#home">Profile</Nav.Link>
           <Nav.Link href="#features">About</Nav.Link>
           <Nav.Link href="#pricing">Admin</Nav.Link>
-        </Nav>
+        </Nav> */}
         {/* <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
           <Button variant="outline-primary">Search</Button>
