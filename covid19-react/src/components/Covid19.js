a71db8c82c85cc475c08afc34b97bda93d8b591e
iTh1nk <fnchao@hotmail.com>
2020-04-02 21:08:51 -0700

    isLoading

--- covid19-react/src/components/Covid19.js
+++ covid19-react/src/components/Covid19.js
@@ -85,6 +85,8 @@ export default function Covid19() {
   return (
     <>
       <Container style={containerStyle}>
+
+      <Loading />
         {/* *********************************************************************************** */}
         {/* Card for Irvine */}
         <Card>
