09e7c2fb0d7d2945042791611c37f05def7e7e84
iTh1nk <fnchao@hotmail.com>
2020-04-18 21:47:51 -0700

    user entity

--- api/src/main/java/com/covid19/api/model/User.java
+++ api/src/main/java/com/covid19/api/model/User.java
@@ -1,5 +1,26 @@
 package com.covid19.api.model;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// @NoArgsConstructor
+// @AllArgsConstructor
+@Entity
+@Data
+// @Table(name = "user")
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String username;
+  private String password;
 }
