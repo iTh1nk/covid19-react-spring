d6d871cd46633fcb1165d499ce503b3808569ec9
iTh1nk <fnchao@hotmail.com>
2020-04-18 18:20:34 -0700

    disable httpsecurity

--- api/src/main/java/com/covid19/api/Application.java
+++ api/src/main/java/com/covid19/api/Application.java
@@ -2,6 +2,9 @@ package com.covid19.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +13,12 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Configuration
+	static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity httpSecurity) throws Exception {
+			httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+		}
+	}
 }
