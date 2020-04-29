1b2abd4caeb8a59fb1af1cc2af67dbefaa8c61bb
iTh1nk <fnchao@hotmail.com>
2020-04-28 19:29:47 -0700

    add PUT to data Irvine

--- covid19-react/src/components/Covid19.js
+++ covid19-react/src/components/Covid19.js
@@ -258,7 +258,7 @@ export default function Covid19() {
                           <td style={numConfirmed}>{item.confirmed}</td>
                           <td style={numNew}>
                             +
-                            {index === dataIrvine.length - 1
+                            {index === 0
                               ? 0
                               : dataIrvine[index + 1].confirmed -
                                 dataIrvine[index].confirmed}
